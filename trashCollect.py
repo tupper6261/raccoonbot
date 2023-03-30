b00157a9831d2703dcfdeb76807b6a6b05f05b7e
tmupper <89988426+tmupper@users.noreply.github.com>
2023-03-30 16:53:26 -0400

    raccoon clone

--- trashCollect.py
+++ trashCollect.py
@@ -201,16 +201,13 @@ async def clone(ctx, prompt: Option(str, "Describe the RaCC0on clone you'd like 
     assignmentEmbed.title = "Loading..."
     assignmentEmbed.description = "This could take up to 5 minutes."
     message = await ctx.respond(embed = assignmentEmbed, view = assignmentView)
-    output = ["a"]
-    '''
     output = replicate.run(
         "doriancollier/raccoon1:831081aba81a2194d5a003eb225d8b2f33b435b6948a3038ca507aa71866abe8",
         input={"prompt": prompt}
     )
-    '''
     print (output)
     assignmentEmbed.title = prompt
-    assignmentEmbed.embed = output[0]
+    assignmentEmbed.image = output[0]
     await message.edit_original_message(embed = assignmentEmbed)
                     
 
